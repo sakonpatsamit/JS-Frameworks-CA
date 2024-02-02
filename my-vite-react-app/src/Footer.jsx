2100266d1aa351c37d10cad7cefbb3c139aaf765
sakonpatsamit <95245596+sakonpatsamit@users.noreply.github.com>
2024-02-02 16:08:48 +0100

    fixed footer

--- my-vite-react-app/src/Footer.jsx
+++ my-vite-react-app/src/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="bg-blue-800 text-white p-4">
-      <div className="container mx-auto px-4 py-4">
+    <footer className="bg-blue-800 text-white p-2 fixed bottom-0 w-full text-xs">
+      <div className="container mx-auto px-4">
         <div className="flex justify-center items-center">
-          <p className="text-sm text-center">
+          <p className="text-center">
             © {new Date().getFullYear()} by Sakon Patsamit. All rights reserved.
           </p>
         </div>
--- my-vite-react-app/src/ProductDetails.jsx
+++ my-vite-react-app/src/ProductDetails.jsx
@@ -93,7 +93,7 @@ export default function ProductDetails() {
           </div>
           <div className="flex justify-between items-center">
             <Link
-              to="/products"
+              to="/"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
               Go Back
